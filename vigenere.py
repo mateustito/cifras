f6d0523c0cf5f4c57d1f87e8ecc6c792cfa1fa41
Luis <luiscarlos.sf@outlook.com>
2019-09-07 22:17:13 -0300

    Add decifragem

--- vigenere.py
+++ vigenere.py
@@ -49,3 +49,24 @@ class Vigenere(Cifra):
             return nova_chave.upper()
         
         return chave.upper()
+    
+    def decifragem(self, cifra, key):
+        alfabeto = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8, 'J':9, 'K':10,'L':11,'M':12, 'N':13, 'O':14,'P':15, 'Q':16,'R':17,'S':18,'T':19,'U':20,'V':21,'W':22,'X':23,'Y':24,'Z':25}
+        _alfabeto = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'}
+        plaintext = ""
+        for index in range(len(cifra)):
+            if (key[index] == ' ') and (cifra[index] == ' '):
+                plaintext += ' '
+                continue
+            key_index = alfabeto[key[index]]
+            cipher_index = alfabeto[cifra[index]]
+            new_index = abs(26 - key_index) + cipher_index
+            if new_index > 25:
+                new_index -= 26
+            plaintext += _alfabeto[new_index]
+        return plaintext
+
+if __name__ == "__main__":
+    v = Vigenere()
+    print(v.decifragem("I EIOGVIZGI PI CY WQEXMYE KDMXFSODENUGW DIAUHM ZE KTEDQ", \
+          "I MEIMEIMEI ME IM EIMEIME IMEIMEIMEIMEI MEIMEI ME IMEIM"))
