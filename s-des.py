0434577a4ca68019f186ac51b04688b274aaed29
Luis <luiscarlos.sf@outlook.com>
2019-10-02 00:23:39 -0300

    Define cabeçalho de funções do SDES

---
+++ s-des.py
@@ -0,0 +1,38 @@
+def text_to_bin(plaintext):
+    pass
+
+def text_to_blocks(bintext):
+    pass
+
+def key_generation(key):
+    pass
+
+def initial_permutation(block):
+    pass
+
+def inverse_permutation(block):
+    pass
+
+def expansion_permutation(block):
+    pass
+
+def exclusive_or(block, subkey):
+    pass
+
+def substitution(block):
+    pass
+
+def permutation(block):
+    pass
+
+def function_k(block, key):
+    pass
+
+def switch_fuction(left_block, right_block):
+    pass
+
+def sdes_encryption(plaintext_block, key):
+    pass
+
+def sdes_decryption(ciphertext_block, key):
+    pass
