81287c7e2971aa9a40d77e31e7e53c4ec31a7704
Luis <luiscarlos.sf@outlook.com>
2019-10-31 23:40:04 -0300

    Define interfaces de funções.

---
+++ rsa.py
@@ -0,0 +1,31 @@
+cartorio = {}
+
+def encryption(e, m, n):
+    return (m ** e) % n
+
+def decryption(d, m, n):
+    return (m ** d) % n
+
+def text_to_blocks():
+    pass
+
+def blocks_to_text():
+    pass
+
+def encrypt_blocks():
+    pass
+
+def decrypt_blocks():
+    pass
+
+def random_number():
+    pass
+
+def public_key():
+    pass
+
+def private_key():
+    pass
+
+if __name__ == "__main__":
+    pass
