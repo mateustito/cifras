ffb1a36c8ff6b652d11822b79de0e9aba6c27cc1
Victor <victor_ps123@live.com>
2019-11-03 13:49:38 -0200

    geracao de chaves publicas

--- rsa.py
+++ rsa.py
@@ -1,5 +1,7 @@
-cartorio = {}
 
+import random #utilizado para geração de numeros randômicos
+
+cartorio = {}
 def encryption(e, m, n):
     return (m ** e) % n
 
@@ -18,14 +20,33 @@ def encrypt_blocks():
 def decrypt_blocks():
     pass
 
-def random_number():
-    pass
+def random_number(bits):
+    n = random.randint(0, pow(2, bits))
+    if n % 2 == 0:
+        n = n + 1
+    while not is_prime(n):
+        n += 2
+    if n == 1:
+        n=n+1
+    return (n)
 
-def public_key():
-    pass
+def is_prime(a):
+    return all(a % i for i in range(2, a))
+
+def public_key(bits):
+    p = random_number(bits)
+    q = random_number(bits)
+    while p == q:
+        q = random_number(bits)
+    n = p*q
+    return (n, p, q)
+    
 
 def private_key():
     pass
 
 if __name__ == "__main__":
-    pass
+    bits = 4 # número de bits da chave
+    n, p, q = public_key(bits) #gera a chave pública
+    
+
