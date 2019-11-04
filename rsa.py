3ed31a217da0bab11daef3bc3cad0cf5188a4a97
Victor <victor_ps123@live.com>
2019-11-04 19:43:47 -0300

    fix errors

--- rsa.py
+++ rsa.py
@@ -1,5 +1,8 @@
-
 import random #utilizado para geração de numeros randômicos
+
+## Chave Publica
+n = 0
+e = 65537 #Para o sistema ficar protegido de alguns tipos de ataque sugere-se este valo = 2^16+1
 
 cartorio = {}
 def encryption(e, m, n):
@@ -20,33 +23,32 @@ def encrypt_blocks():
 def decrypt_blocks():
     pass
 
-def random_number(bits):
-    n = random.randint(0, pow(2, bits))
+def random_number():
+    n = random.randint(0, pow(10, 100))
     if n % 2 == 0:
         n = n + 1
     while not is_prime(n):
         n += 2
     if n == 1:
-        n=n+1
+        n = n + 1
     return (n)
 
 def is_prime(a):
     return all(a % i for i in range(2, a))
 
-def public_key(bits):
-    p = random_number(bits)
-    q = random_number(bits)
+def public_key():
+    p = random_number()
+    q = random_number()
     while p == q:
-        q = random_number(bits)
-    n = p*q
-    return (n, p, q)
+        q = random_number()
+    _n = p*q
+    return _n
     
 
 def private_key():
     pass
 
 if __name__ == "__main__":
-    bits = 4 # número de bits da chave
-    n, p, q = public_key(bits) #gera a chave pública
+    n = public_key() #gera a chave pública
     
 
