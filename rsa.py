45e70804d3fbbfd56bc4745a2c5b5291a45d9f99
Victor <victor_ps123@live.com>
2019-11-04 21:01:00 -0300

    add test primalidade Miller-Rabin

--- rsa.py
+++ rsa.py
@@ -27,23 +27,47 @@ def random_number():
     n = random.randint(0, pow(10, 100))
     if n % 2 == 0:
         n = n + 1
-    while not is_prime(n):
+    while not is_probable_prime(n, 64):
         n += 2
     if n == 1:
         n = n + 1
     return (n)
-
-def is_prime(a):
-    return all(a % i for i in range(2, a))
 
 def public_key():
     p = random_number()
     q = random_number()
     while p == q:
         q = random_number()
+    print(p)
+    print(q)    
     _n = p*q
     return _n
-    
+
+def is_probable_prime(n,k=40):
+	#Teste de primalidade Miller-Rabin 
+	# k = 40 numero recomendado de rounds de teste
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
 
 def private_key():
     pass
